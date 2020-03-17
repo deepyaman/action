36035612e3a802f5f8e97ed285e97e44ed7ae955
Deepyaman Datta <deepyaman.datta@utexas.edu>
2020-03-17 18:30:51 -0500

    Support `args` keyword in pre-commit GitHub action

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
+const tr = require('@actions/exec/lib/toolrunner');
 
-const ARGS = [
-    'run', '--all-files', '--show-diff-on-failure', '--color=always'
-];
+let args = core.getInput('args');
+if (!args) {
+    args = [
+        'run', '--all-files', '--show-diff-on-failure', '--color=always'
+    ];
+} else {
+    args = [
+        'run', ...tr.argStringToArray(args), '--show-diff-on-failure', '--color=always'
+    ];
+}
 
 function addToken(url, token) {
     return url.replace(/^https:\/\//, `https://x-access-token:${token}@`)
@@ -19,12 +27,12 @@ async function main() {
     const token = core.getInput('token');
     const pr = github.context.payload.pull_request;
     const push = !!token && !!pr;
-    const ret = await exec.exec('pre-commit', ARGS, {ignoreReturnCode: push});
+    const ret = await exec.exec('pre-commit', args, {ignoreReturnCode: push});
     if (ret && push) {
         // actions do not run on pushes made by actions.
         // need to make absolute sure things are good before pushing
         // TODO: is there a better way around this limitation?
-        await exec.exec('pre-commit', ARGS);
+        await exec.exec('pre-commit', args);
 
         const diff = await exec.exec(
             'git', ['diff', '--quiet'], {ignoreReturnCode: true}
